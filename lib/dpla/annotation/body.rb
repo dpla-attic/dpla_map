037ef548636c0b1b3d1b2cae3f4b0308fac3d8bb
Tom Johnson <thomas.johnson@oregonstate.edu>
2014-09-19 10:41:05 -0700

    Fixing text property on annotation bodies
    Annotation bodies were using the cnt namespace as a property, this is
    corrected to use cnt:chars.

--- lib/dpla/annotation/body.rb
+++ lib/dpla/annotation/body.rb
@@ -2,7 +2,7 @@ module DPLA::Annotation
   class Body < ActiveTriples::Resource
     configure :type => RDF::CNT.ContentAsText
 
-    property :text, :predicate => RDF::CNT
+    property :text, :predicate => RDF::CNT.chars
     property :language, :predicate => RDF::DC.language, :class_name => 'DPLA::Language'
     property :format, :predicate => RDF::DC11.format
   end
